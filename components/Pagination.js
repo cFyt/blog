999fee139b6b16c9cc67cc46e29930530c1e98b4
cFyt <45104771+cFyt@users.noreply.github.com>
2022-01-23 20:23:06 +0300

    Update Pagination.js

--- components/Pagination.js
+++ components/Pagination.js
@@ -9,7 +9,7 @@ export default function Pagination({ totalPages, currentPage }) {
       <nav className="flex justify-between">
         {!prevPage && (
           <button rel="previous" className="cursor-auto disabled:opacity-50" disabled={!prevPage}>
-            Previous
+            Önceki
           </button>
         )}
         {prevPage && (
@@ -22,7 +22,7 @@ export default function Pagination({ totalPages, currentPage }) {
         </span>
         {!nextPage && (
           <button rel="next" className="cursor-auto disabled:opacity-50" disabled={!nextPage}>
-            Next
+            Sonraki
           </button>
         )}
         {nextPage && (
